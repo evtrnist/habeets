055f8f0b09dff51fae1871cc9d7b3be2ae8c923b
evtrnist <evagrandet@ro.ru>
2023-08-05 13:20:23 +0400

    adds markup for logging in

--- apps/habeets/src/app/auth/login/login-card/login-card.component.ts
+++ apps/habeets/src/app/auth/login/login-card/login-card.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { TuiValidationError } from '@taiga-ui/cdk';
 
 @Component({
   selector: 'habeets-login-card',
   templateUrl: './login-card.component.html',
   styleUrls: ['./login-card.component.less'],
 })
-export class LoginCardComponent {}
+export class LoginCardComponent {
+  public readonly loginFormGroup = new FormGroup({
+    email: new FormControl('', {
+      validators: [this.customRequiredValidator('Please enter your email')],
+      updateOn: 'blur'
+    }),
+    password: new FormControl('', {
+      validators: [this.customRequiredValidator('Please enter your password')],
+      updateOn: 'blur'
+    }),
+  });
+
+  private customRequiredValidator(error = ''): (field: AbstractControl) => ValidationErrors | null {
+    return (field: AbstractControl): ValidationErrors | null =>
+      field.value ? null : { required: this.getRequiredError(error) };
+  }
+
+  private getRequiredError(error: string): TuiValidationError {
+    return new TuiValidationError(error);
+  }
+}
--- apps/habeets/src/app/auth/login/login-card/login-card.module.ts
+++ apps/habeets/src/app/auth/login/login-card/login-card.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TuiFieldErrorPipeModule, TuiInputModule } from '@taiga-ui/kit';
 import { LoginCardComponent } from './login-card.component';
+import { TuiButtonModule, TuiErrorModule, TuiLinkModule } from '@taiga-ui/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [LoginCardComponent],
-  imports: [CommonModule],
-  exports: [LoginCardComponent]
+  imports: [CommonModule, RouterModule, ReactiveFormsModule, TuiInputModule, TuiErrorModule, TuiFieldErrorPipeModule, TuiLinkModule, TuiButtonModule],
+  exports: [LoginCardComponent],
 })
 export class LoginCardModule {}
