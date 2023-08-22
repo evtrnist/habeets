17ee6f38ff3238a17858f01d22a1c28c71dc38b9
evtrnist <evagrandet@ro.ru>
2023-08-22 20:49:18 +0400

    adds imports

--- apps/habeets/src/app/auth/signup/signup-card/signup-card.module.ts
+++ apps/habeets/src/app/auth/signup/signup-card/signup-card.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignupCardComponent } from './signup-card.component';
+import { TuiButtonModule, TuiErrorModule, TuiLinkModule } from '@taiga-ui/core';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TuiFieldErrorPipeModule, TuiInputModule } from '@taiga-ui/kit';
 
 @NgModule({
   declarations: [SignupCardComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    TuiInputModule,
+    TuiErrorModule,
+    TuiFieldErrorPipeModule,
+    TuiLinkModule,
+    TuiButtonModule,
+  ],
+  exports: [SignupCardComponent],
 })
 export class SignupCardModule {}
