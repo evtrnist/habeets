c8893d069e280fe67a653178fe3afc92622202c4
evtrnist <evagrandet@ro.ru>
2023-08-23 22:42:23 +0400

    adds form group

--- apps/habeets/src/app/auth/signup/signup-card/signup-card.component.ts
+++ apps/habeets/src/app/auth/signup/signup-card/signup-card.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'habeets-signup-card',
   templateUrl: './signup-card.component.html',
   styleUrls: ['./signup-card.component.less'],
 })
-export class SignupCardComponent {}
+export class SignupCardComponent {
+  public signupFormGroup: FormGroup
+}
