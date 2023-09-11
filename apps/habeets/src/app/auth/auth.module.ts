f0c6ebbcbb06f04d34299bcb237715e3153b341c
evtrnist <evagrandet@ro.ru>
2023-09-11 22:26:40 +0400

    deletes imports

--- apps/habeets/src/app/auth/auth.module.ts
+++ apps/habeets/src/app/auth/auth.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [AuthComponent],
-  imports: [CommonModule, AuthRoutingModule, LoginModule],
+  imports: [CommonModule, AuthRoutingModule],
 })
 export class AuthModule {}
